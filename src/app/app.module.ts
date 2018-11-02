fb69077c3319f98050f56a2a1605ff5fdd3419f4
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-02 15:08:28 -0300

    Creating Painel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {TopoComponent} from '../app/topo/Topo.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule
