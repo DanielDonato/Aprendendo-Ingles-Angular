e4d78e80a92566f205e78be9f166054b9ff8bf26
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-02 15:04:54 -0300

    Creating Component Topo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TopoComponent} from '../app/topo/Topo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule
