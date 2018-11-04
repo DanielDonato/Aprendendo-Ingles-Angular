4dc446f0b15a93878bbbd325c5ac5552675295d7
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-04 18:15:38 -0200

    Creating class Frase

---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,9 @@
+export class Frase {
+    public fraseEng:string;
+    public frasePtBr:string;
+
+    public constructor(fraseEng:string, frasePtBr:string){
+        this.fraseEng = fraseEng;
+        this.frasePtBr = frasePtBr;
+    }
+}
