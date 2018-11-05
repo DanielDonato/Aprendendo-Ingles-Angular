4085fe80eb7fc494dd8d0872699253bb0b42194b
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-05 15:18:07 -0200

    Logica para trocar as frases

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -8,18 +8,35 @@ import {FRASES} from "./frases-mock";
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-
+  
   public frases: Frase[] = FRASES;
   public instrucao:string = "Traduza a frase:"
   public resposta:string;
-  constructor() {}
+
+  public rodada:number = 0;
+  public rodadaFrase: Frase
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit() {
   }
 
   public atualizaReposta(resposta:Event):void {
     this.resposta = ((<HTMLInputElement>resposta.target).value);
-    console.log(this.resposta);
+    //console.log(this.resposta);
+  }
+
+  public verificarResposta():void{
+    if(this.resposta === this.rodadaFrase.frasePtBr){
+      this.rodada++;
+      console.log(this.rodada);
+      this.rodadaFrase = this.frases[this.rodada];
+    }else {
+      alert("Frase incorreta");
+    }
   }
 
 }
